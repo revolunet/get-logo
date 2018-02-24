c602aa0056409a5cedd54a07c3ef7fd3153b4172
Julien Bouquillon <julien@revolunet.com>
2018-02-24 05:04:12 +0100

    move to /src

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ const normalizeName = name =>
     .replace(/\s/g, "-")
     .replace(/_/g, "-");
 
-const extensions = {
+const extensions2 = {
   ".png": "image/png",
   ".gif": "image/gif",
-  ".jpg": "image/jpeg",
+  ".jpg": "image/Q",
   ".jpeg": "image/jpeg",
   ".svg": "image/svg+xml"
 };
 
 const makeBase64Url = (contentType, b64) => `data:${contentType};base64,${b64}`;
 
-const getBase64FromUrl = url =>
+const getBase64FromUrl = async url =>
   fetch(url)
     .then(async r => ({
       contentType: r.headers.get("content-type").split(";")[0],
@@ -25,10 +25,16 @@ const getBase64FromUrl = url =>
     .then(({ contentType, buffer }) => makeBase64Url(contentType, buffer.toString("base64")));
 
 const getFromGithub = name => () =>
-  getBase64FromUrl(`https://rawgithub.com/gilbarbara/logos/master/logos/${normalizeName(name)}.svg`);
+  getBase64FromUrl(
+    `https://rawgithub.com/gilbarbara/logos/master/logos/${normalizeName(name)}.svg`
+  );
 
 const getFromQwant = name => () =>
-  fetch(`https://api.qwant.com/api/search/images?count=10&offset=1&q=logo%20svg%20${normalizeName(name)}&size=small`)
+  fetch(
+    `https://api.qwant.com/api/search/images?count=10&offset=1&q=logo%20svg%20${normalizeName(
+      name
+    )}&size=small`
+  )
     .then(r => {
       if (r.status === 200) {
         return r.json();
@@ -36,7 +42,11 @@ const getFromQwant = name => () =>
       throw new Error(r.status);
     })
     .then(json => {
-      const result = json.data.result && json.data.result.items && json.data.result.length && json.data.result.items[0];
+      const result =
+        json.data.result &&
+        json.data.result.items &&
+        json.data.result.length &&
+        json.data.result.items[0];
       if (!result) {
         throw new Error("No results");
       }
@@ -46,8 +56,8 @@ const getFromQwant = name => () =>
 const getLogo = name =>
   Promise.resolve()
     .then(getFromGithub(name))
-    .catch(getFromQwant(name));
-    .catch(e => null)
+    .catch(getFromQwant(name))
+    .catch(e => null);
 
 module.exports = getLogo;
 
